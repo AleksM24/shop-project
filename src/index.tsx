f1bf2f1a71072a327cedabcf180fc5641cfa2d0d
Oleksandr <aleksandrmigay@gmail.com>
2023-01-26 18:23:38 +0200

    add react components

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-//jsx
-const h1 = <h1 id="title" className='new'>Hello React.js</h1> 
+const Title = () => {
+    return <h1>Hello App.js</h1>
+}
 
-let a = 10;
-let b = 'Hello test'
-
-const list = (
-    <ul>
-        <li>List item {a + 10 + 15}</li>
-        <li>List item {b}</li>
-        <li>List item 3</li>
-    </ul>
-)
-
-const content = (
+const Content = () => {
+    return (
     <div>
-        {h1}
-        {list}
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, id! Dolore impedit odit ad. Reprehenderit officiis tempore voluptatem? Corporis incidunt earum cum saepe asperiores laudantium! Atque, consectetur quo. Possimus, aspernatur.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores quisquam, illum recusandae dolore earum autem est eius dicta iure nostrum illo harum vero voluptatum quos id veniam, cumque asperiores quod!</p>
     </div>
-)
+    )
+}
+
+function App (){
+    return (
+        <div>
+            <Title />
+            <Content />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{content}</React.StrictMode>)
+root.render(<React.StrictMode>
+    <App />
+</React.StrictMode>)
