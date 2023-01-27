78436d85ac74fcf686785dc8b260c01e159a9519
Oleksandr <aleksandrmigay@gmail.com>
2023-01-27 19:03:58 +0200

    removed files css and contetnt, insert an absolute imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,8 @@
-import { type } from '@testing-library/user-event/dist/type'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Content} from './Content'
-import Title from './Title'
 
-function App() {
-    return (
-        <>
-            <Title text="React" />
-            <Title text="TS" />
-            <Content 
-            text1={'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum voluptates aliquid corporis vel libero vitae dolor, a minima impedit mollitia commodi ipsam soluta neque corrupti, non aspernatur provident? Maiores, eligendi?'} 
-            text2={'Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis perferendis quibusdam, magni alias autem iste cum vitae id delectus ipsam minus at soluta earum architecto dolorum ratione magnam fugit distinctio.'} 
-            year={2023} />
-        </>
-    )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <React.StrictMode></React.StrictMode>
 )
