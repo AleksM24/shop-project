40acfe11ceff6bf007927492d72bb0f247aaaab0
Oleksandr <aleksandrmigay@gmail.com>
2023-02-03 16:49:03 +0200

    add props to ProductsListItem

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -2,6 +2,7 @@ import { Grid, Typography } from '@mui/material'
 import ProductsListItem from './ProductsListItem'
 
 type Props = {}
+
 const ProductsList = (props: Props) => {
     return (
         <>
@@ -12,15 +13,39 @@ const ProductsList = (props: Props) => {
             <Grid container spacing={3}>
                 <Grid item xs={12} sm={6} md={4}>
                     {' '}
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title={'iPhone 14 Pro'}
+                        description={
+                            'iPhone 14 Pro.'
+                        }
+                        type={'Phone'}
+                        capacity={'256 Gb'}
+                        price={1200}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
                     {' '}
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title={'Samsung Galaxy'}
+                        description={
+                            'Samsung Galaxy.'
+                        }
+                        type={'Tablet'}
+                        capacity={'128 Gb'}
+                        price={1000}
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
                     {' '}
-                    <ProductsListItem />
+                    <ProductsListItem
+                        title={'Redmi Note 10'}
+                        description={
+                            'Redmi Note 10.'
+                        }
+                        type={'Phone'}
+                        capacity={'64 Gb'}
+                        price={500}
+                    />
                 </Grid>
             </Grid>
         </>
