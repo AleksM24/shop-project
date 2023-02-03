8e52193c5e50366fe5c048472bcf8f75fad9cf85
Oleksandr <aleksandrmigay@gmail.com>
2023-02-03 16:55:55 +0200

    modifide ProductListItem with destructurisation

--- src/components/ProductsList/ProductsListItem.tsx
+++ src/components/ProductsList/ProductsListItem.tsx
@@ -9,15 +9,21 @@ type Props = {
     price: number
 }
 
-const ProductsListItem = (props: Props) => {
+const ProductsListItem = ({
+    title,
+    description,
+    type,
+    capacity,
+    price,
+}: Props) => {
     return (
         <Card className="product" variant="outlined">
             <CardContent>
-                <div className="product-title">{props.title}</div>
-                <div className="product-desc"> This is {props.description}</div>
-                <div className="product-features"> Type: {props.type}</div>
-                <div className="product-features">Capacity: {props.capacity}</div>
-                <div className="product-price">{props.price}$</div>
+                <div className="product-title">{title}</div>
+                <div className="product-desc"> This is {description}</div>
+                <div className="product-features"> Type: {type}</div>
+                <div className="product-features">Capacity: {capacity}</div>
+                <div className="product-price">{price}$</div>
             </CardContent>
 
             <CardActions className="product-btn-wrap">
